c9f2bd22e124b201ee72e2d38c2073586c36946d
pckurdu <39583402+pckurdu@users.noreply.github.com>
2019-06-25 13:27:21 +0300

    Add files via upload

---
+++ client/src/components/BookList.js
@@ -0,0 +1,54 @@
+//import to create component
+import React,{Component} from 'react';
+
+//query for gql module
+import {gql} from 'apollo-boost';
+
+//for export
+import {graphql} from 'react-apollo';
+
+//book list query
+const getBooksQuery=gql `
+
+{
+    books{
+        name
+        type
+        id
+    }
+}
+`
+
+class BookList extends Component{
+    getBooks(){
+        //We access the books with the data object.
+        var datas=this.props.data;
+
+        //if books are gets from mlab
+        if(datas.loading){
+            return (<div>Books Loading</div>)
+        }
+        //when getting books is finished
+        else{
+            return datas.books.map(book=>{
+                return(
+                    <li key={book.id}>{book.name}</li>
+                )
+            })
+        }
+    }
+    render(){
+        console.log(this.props);
+        
+        return(
+            <div>
+                <ul id="book-list">
+                    {/* use getBooks function */}
+                    {this.getBooks()}
+                </ul>
+            </div>
+        )
+    }
+}
+//export graphql
+export default  graphql(getBooksQuery) (BookList);
